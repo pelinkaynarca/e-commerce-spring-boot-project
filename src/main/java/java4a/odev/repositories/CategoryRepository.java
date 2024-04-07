d8f8e7c35c1f5861dd658a42a73886f0b9a90d26
omerfarukbala <omerfarukbala@gmail.com>
2024-04-07 16:15:59 +0300

    feat: add query methods.

--- src/main/java/java4a/odev/repositories/CategoryRepository.java
+++ src/main/java/java4a/odev/repositories/CategoryRepository.java
@@ -1,9 +1,26 @@
 package java4a.odev.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java4a.odev.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+	Optional<Category> findByNameIgnoreCase(String name);
 
+	List<Category> findByNameLikeIgnoreCase(String name);
+
+	/**
+	 * Verilen kategorinin alt kategorilerini listeler.
+	 * 
+	 * @param parentId
+	 * @return alt kategoriler
+	 */
+	@Query(value = "select c from Category c where c.category.id in (:parentId)")
+	List<Category> findByParentId(@Param("parentId") Optional<Integer> parentId);
+//	List<Category> findByCategory(Category category);
 }
--- src/main/java/java4a/odev/repositories/ProductRepository.java
+++ src/main/java/java4a/odev/repositories/ProductRepository.java
@@ -1,9 +1,54 @@
 package java4a.odev.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java4a.odev.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+	Optional<Product> findByNameIgnoreCase(String name);
+
+	List<Product> findByNameLikeIgnoreCase(String name);
+
+	/**
+	 * Fiyat aralığına göre listeler.
+	 * 
+	 * @param p1
+	 * @param p2
+	 * @return ürün listesi
+	 */
+	List<Product> findByUnitPriceBetween(double p1, double p2);
+
+	/**
+	 * Verilen kategorideki ürünleri listeler.
+	 * 
+	 * @param categoryId
+	 * @return ürün listesi
+	 */
+	@Query("select p from Product p where p.category.id = :categoryId")
+	List<Product> findByCategoryId(@Param("categoryId") int categoryId);
+
+	/**
+	 * Yeni eklenen ürünleri listeler.
+	 * 
+	 * @param limit listelenecek ürün sayısı
+	 * @return ürün listesi
+	 */
+	@Query("select p from Product p order by p.createdAt desc limit :limit")
+	List<Product> findByCreatedAtDesc(@Param("limit") int limit);
+
+	/**
+	 * Satış sayısına göre en çok satan ürünleri listeler
+	 * 
+	 * @param limit listelenecek ürün sayısı
+	 * @return ürün listesi
+	 */
+	@Query(value = "SELECT p.* FROM products p" + " JOIN order_items oi ON p.id = oi.product_id" + " GROUP BY p.id"
+			+ " ORDER BY SUM(oi.quantity) DESC" + " LIMIT :limit", nativeQuery = true)
+	List<Product> findBestsellersByQuantity(@Param("limit") int limit);
 
 }

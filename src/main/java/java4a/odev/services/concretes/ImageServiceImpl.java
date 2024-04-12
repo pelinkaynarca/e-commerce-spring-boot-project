e96c6599e8d588b20b86d6f3ab4c987491b98de6
gurka <gurkandogan91@gmail.com>
2024-04-12 15:43:47 +0300

    fix: image update issue

--- src/main/java/java4a/odev/services/concretes/ImageServiceImpl.java
+++ src/main/java/java4a/odev/services/concretes/ImageServiceImpl.java
@@ -15,43 +15,38 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
 @Service
 @AllArgsConstructor
 public class ImageServiceImpl implements ImageService {
 
     private ImageRepository imageRepository;
 
-
     @Override
     public List<ListImageResponse> getByProductId(int productId) {
-        List<Image> image = imageRepository.findAll();
-        return ImageMapper.INSTANCE.ListResponseFromImage(image);
-
+        List<Image> images = imageRepository.findAll();
+        return ImageMapper.INSTANCE.ListResponseFromImage(images);
     }
 
     @Override
-        public ListImageResponse getById(int id) {
-            Image image = imageRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException("Image not found with id: " + id));
-            return ImageMapper.INSTANCE.ListImageResponse(image);
-        }
+    public ListImageResponse getById(int id) {
+        Image image = imageRepository.findById(id).orElseThrow(() -> new RuntimeException("Image not found with id: " + id));
+        return ImageMapper.INSTANCE.ListImageResponse(image);
+    }
 
     @Override
     public AddImageResponse add(AddImageRequest request) {
         Image image = ImageMapper.INSTANCE.imageFromAddRequest(request);
         image = imageRepository.save(image);
-       return ImageMapper.INSTANCE.addResponseFromImage(image);
+        return ImageMapper.INSTANCE.addResponseFromImage(image);
 
     }
 
     @Override
     public UpdateImageResponse update(UpdateImageRequest request) {
-        imageRepository.findById(request.getProductId())
-                .orElseThrow(() -> new RuntimeException("Country not found with id: " + request.getId()));
+        imageRepository.findById(request.getId()).orElseThrow(() -> new RuntimeException("Image not found with id: " + request.getId()));
         Image image;
-       image = ImageMapper.INSTANCE.imageFromUpdateRequest(request);
-       image = imageRepository.save(image);
+        image = ImageMapper.INSTANCE.imageFromUpdateRequest(request);
+        image = imageRepository.save(image);
 
         UpdateImageResponse updateImageResponse = ImageMapper.INSTANCE.updateResponseFromImage(image);
         return updateImageResponse;

7c3c7f354f3aa7ef6024c8882e7afb168b6ef93a
Pelin <kaynarcapelin@hotmail.com>
2024-04-12 13:37:36 +0300

    feat: add cityWithSameNameShouldNotExist method in CityServiceImpl

--- src/main/java/java4a/odev/repositories/CityRepository.java
+++ src/main/java/java4a/odev/repositories/CityRepository.java
@@ -1,9 +1,12 @@
 package java4a.odev.repositories;
 
+import java4a.odev.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java4a.odev.entities.City;
 
-public interface CityRepository extends JpaRepository<City, Integer> {
+import java.util.Optional;
 
+public interface CityRepository extends JpaRepository<City, Integer> {
+    Optional<City> findByNameIgnoreCase(String name);
 }

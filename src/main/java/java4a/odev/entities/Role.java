cb6906a58dd2edb2ff15cee92cc7d50c301bafed
omerfarukbala <omerfarukbala@gmail.com>
2024-04-13 20:25:15 +0300

    refactor: added hashCode() and equals() override impls for role entity

--- src/main/java/java4a/odev/entities/Role.java
+++ src/main/java/java4a/odev/entities/Role.java
@@ -1,5 +1,7 @@
 package java4a.odev.entities;
 
+import java.util.Objects;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -20,4 +22,21 @@ public class Role {
     @Column(nullable = false, unique = true)
     @Enumerated(EnumType.STRING)
     private RoleName name;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return id == other.id;
+	}
 }

c1b691a6f3753ca3ba5e3e261c29abb05685e869
omerfarukbala <omerfarukbala@gmail.com>
2024-04-13 15:19:25 +0300

    refactor: added ignorecase to user search by email

--- src/main/java/java4a/odev/repositories/UserRepository.java
+++ src/main/java/java4a/odev/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java4a.odev.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	Optional<User> findByEmail(String email);
+	Optional<User> findByEmailIgnoreCase(String email);
 }
